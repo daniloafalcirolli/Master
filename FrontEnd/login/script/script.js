ec80507595ae6e57b875ec0fe5829edce3e804a2
Matheus Belarmino Pignata <pignatamatheus@gmail.com>
2022-04-06 21:31:27 -0300

    pignata

--- FrontEnd/login/script/script.js
+++ FrontEnd/login/script/script.js
@@ -49,3 +49,9 @@ function maskCPF(i){
             console.log(err);
         });
 }
+
+const btn_dm = document.getElementById('btn_dm');
+
+btn_dm.addEventListener("change", (e) =>{
+    document.body.classList.toggle('dark', e.target.checked);
+})
